c5c50c219c68e47208a8defb96fa9486b527d337
aj96818 <aj96818@gmail.com>
2020-12-10 20:21:56 -0500

    adding stock_ticker_list.py to repo.

---
+++ stock_ticker_list.py
@@ -0,0 +1,42 @@
+# create list of stocks from downloaded TXT file of all 3k+ stocks from eoddata.com
+
+path = '//Users/alanjackson/Documents/Environments/stocks_env/NYSE.txt'
+
+path2 = '//Users/alanjackson/Documents/Environments/stocks_env/stock_data_full_3k.csv'
+
+
+
+
+# Stocks downloaded from 'www.eoddata.com' on 12/9/2020
+
+path = '//Users/alanjackson/Documents/Environments/stocks_env/NYSE.txt'
+
+file = open(path, 'r')
+
+tickers = []
+
+for aline in file:
+	values = aline.split() 
+	tickers.append(values[0])
+
+file.close()
+
+
+
+
+
+
+
+
+
+file = open(path2, 'r')
+
+i = 0
+for aline in file:
+	i += 1
+
+print(i)
+
+file.close()
+
+
