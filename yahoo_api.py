c6c14261dc5f4c503bbfe4dca4ecbbbb003c92fc
aj96818 <aj96818@gmail.com>
2020-12-11 16:03:57 -0500

    Mac version updated on 12/11

--- yahoo_api.py
+++ yahoo_api.py
@@ -10,8 +10,17 @@
 import pandas as pd
 from yahoo_fin.stock_info import get_data
 
-tickers = ['SNE','TAP','SNOW','F','R','NOW','TTD','ADBE','DDOG','FSLY','RAMP','DEM','WIX','SEDG','A','ETSY','PINS','FVRR','AAPN','LI','LYFT','UBER','DFS','DGS','HD','LUV','DIA','CRM','AMD','SNAP','TWTR','NVDA','FB','AAPL','SPLK', 'TWLO', 'AMZN', 'MSFT', 'NFLX', 'NIO', 'WKHS', 'NKLA', 'PRTK', 'EIGR', 'ATRA', 'VKTX', 'VXRT', 'JNJ', 'NVAX', 'AZN', 'INO', 'MRNA', 'TTNP', 'BA', 'SNOW', 'FSLR', 'JOBS', 'APPS', 'SRNE', 'OM', 'CRNC', 'CDLX', 'EBON', 'ATEX']
-companies = ['Sony','Molsen Coors Beverage', 'Snowflake','Ford','Ryder','ServiceNow','Trade Desk','Adobe','DataDog','Fastly','LiveRamp','WisdomTree Emerging Markets High Dividend ETF','Wix','SolarEdge','Agilent','Etsy','Pinterest','Fiverr International','Appian','Li Auto','LYFT','Uber','Discover','WisdomTree Emerging Markets SmallCap Dividend ETF','Home Depot','Southwest Airlines','SPDR Dow Jones Industrial Avg ETF','Salesforce','AMD','Snapchat','Twitter','Nvidia','Facebook','Apple','Splunk','Twilio', 'Amazon', 'Microsoft', 'Netflix', 'Nio (Electric Car)', 'Workhorse Group Inc', 'Nikola Corp',           'Paratek Pharma', 'Eiger Biopharm', 'Atara biotherapeutics', 'Viking Therapeutics', 'Vaxart Pharma', 'Johnson&Johnson', 'Novavax Pharm', 'AstraZeneca Pharm', 'Inovio Pharma', 'Moderna Pharma', 'Titan Pharma', 'Boeing', 'Snowflake', 'First Solar', '51job', 'Digital Turbine', 'Sorento Therapeutics', 'Outset Medical', 'Cerence', 'Cardlytics', 'Ebang', 'Anterix']
+path = '//Users/alanjackson/Documents/Environments/stocks_env/NYSE.txt'
+
+file = open(path, 'r')
+
+tickers = []
+
+for aline in file:
+	values = aline.split() 
+	tickers.append(values[0])
+
+file.close()
 
 historical_data = {}
 for ticker in tickers:
@@ -26,7 +35,7 @@ for df in historical_data:
 
 final_df = pd.concat(out, ignore_index = False)
 
-#final_df.to_csv(r'stock_data_full.csv')
+final_df.to_csv(r'//Users/alanjackson/Documents/Environments/stocks_env/stock_data_full_3k.csv')
 
 
 
