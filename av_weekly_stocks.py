45ed102b4b59b9d2dd001c70bcff83e51a2a0ca0
aj96818 <aj96818@gmail.com>
2020-12-11 21:53:45 -0500

    added try/except to fix KeyError with dictionary.

--- av_weekly_stocks.py
+++ av_weekly_stocks.py
@@ -21,50 +21,43 @@ for aline in file:
 
 file.close()
 
-
-#tickers = ['SNE', 'TAP']
-
-#,'TAP','SNOW','F','R','NOW','TTD','ADBE','DDOG']
-# 'FSLY','RAMP','DEM','WIX','SEDG','A','ETSY','PINS','FVRR','AAPN','LI',
-# 'LYFT','UBER','DFS','DGS','HD','LUV','DIA','CRM','AMD','SNAP','TWTR','NVDA',
-# 'FB','AAPL','SPLK', 'TWLO', 'AMZN', 'MSFT', 'NFLX', 'NIO', 'WKHS', 'NKLA',
-#  'PRTK', 'EIGR', 'ATRA', 'VKTX', 'VXRT', 'JNJ', 'NVAX', 'AZN', 'INO', 'MRNA',
-#   'TTNP', 'BA', 'SNOW', 'FSLR', 'JOBS', 'APPS', 'SRNE', 'OM', 'CRNC', 'CDLX',
-#    'EBON', 'ATEX']
+#tickers = ['SNE', 'TAP','TAP','SNOW','F','R','NOW','TTD','ADBE','DDOG', 'FSLY','RAMP','DEM','WIX','SEDG','A','ETSY','PINS','FVRR','AAPN','LI']
 
 master_df = pd.DataFrame()
 df_list = []
 for ticker in tickers:
-
-	API_URL = "https://www.alphavantage.co/query" 
-	data = { 
-	    "function": 'TIME_SERIES_WEEKLY_ADJUSTED', 
-	    "symbol": ticker,
-	    "outputsize" : "compact",
-	    "datatype": "json", 
-	    "apikey": 'W1U7T09FFM4DY97N'} 
+	try:
+		API_URL = "https://www.alphavantage.co/query" 
+		data = { 
+	    	"function": 'TIME_SERIES_WEEKLY_ADJUSTED', 
+	    	"symbol": ticker,
+	    	"outputsize" : "compact",
+	    	"datatype": "json", 
+	    	"apikey": 'W1U7T09FFM4DY97N'} 
 	
-	response = requests.get(API_URL, data) 
-	response_json = response.json()
+		response = requests.get(API_URL, data) 
+		response_json = response.json()
 
-	x = json.dumps(response_json)
-	d = json.loads(x)
-	
-	date_list = []
-	data_list = []
+		x = json.dumps(response_json)
+		d = json.loads(x)
+		
+		date_list = []
+		data_list = []
 
-	for line in d['Weekly Adjusted Time Series'].items():
- 		date, data = line
- 		date_list.append(date)
- 		data_list.append(data)
+		for line in d['Weekly Adjusted Time Series'].items():
+			date, data = line
+			date_list.append(date)
+			data_list.append(data)
 
-	df_date = pd.DataFrame(date_list)
-	df_data = pd.DataFrame(data_list)
+		df_date = pd.DataFrame(date_list)
+		df_data = pd.DataFrame(data_list)
 
-	df = pd.concat([df_date, df_data], axis = 1)
-	df['Symbol'] = ticker
-	df_list.append(df)
-	time.sleep(2)
+		df = pd.concat([df_date, df_data], axis = 1)
+		df['Symbol'] = ticker
+		df_list.append(df)
+		time.sleep(2)
+	except:
+		pass
 
 master_df = pd.concat(df_list, ignore_index = False)
 master_df.columns = ['date', 'open', 'high', 'low', 'close', 'adj close', 'volume', 'dividend', 'symbol']
