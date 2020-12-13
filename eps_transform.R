83c229b152b13ae0ca7896d9626b2335358203e0
aj96818 <aj96818@gmail.com>
2020-12-13 14:30:55 -0500

    transfored EPS earnings data from finhub from long to wide format.

--- eps_transform.R
+++ eps_transform.R
@@ -14,22 +14,30 @@ dupes<-eps_data[c('unique_row')]
 eps_data<-eps_data[!duplicated(dupes),]
 eps_list <- split(eps_data, eps_data$symbol)
 
+
 pvt_wide_func <- function(df){
   myvars <- c('unique_row', 'actual', 'period')
   df <- df[myvars]
   pv_wide_df <- tidyr::pivot_wider(df, names_from = period, values_from = actual)
-  return(pv_wide_df)
+  #pv_wide_df <- reshape::melt(as.matrix(pv_wide_df), na.rm = T)
+  pv_wide_df$symbol <- gsub('[[:digit:]]+', '', pv_wide_df$unique_row)
+  pv_wide_df <- pv_wide_df[, !(colnames(pv_wide_df) == 'unique_row')]
+  pv_final <- pv_wide_df %>%
+    group_by(symbol) %>%
+    summarise_all(funs(first(.[!is.na(.)])))
+  return(pv_final)
   }
 
 pvt_wide_list <- lapply(eps_list, pvt_wide_func)
+
 df_wide <- bind_rows(pvt_wide_list)
 
+
 setwd('~/Documents/Environments/stocks_env/')
+
+head(df_wide)
+
 write_csv(eps_wide, 'eps_wide.csv')
-
-# drop na's ...
-
-# rename columns
 
 # add calc fields
 
