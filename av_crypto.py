dd5d6b6a236e998b44ec123aebe95bdd888567d5
aj96818 <aj96818@gmail.com>
2020-12-31 10:49:23 -1000

    CSV file output created.  Need to add functionality to append new records to existing CSV.

--- av_crypto.py
+++ av_crypto.py
@@ -11,10 +11,9 @@ import json
 startTime = time.time()
 
 
-crypto = ['BTC', 'ETH', 'ZRX', 'BAL', 'BAT', 'BCH', 'KNC', 'LSK', 'IOTA', 'MRPH', 'NEO',
-'ONT', 'DOT', 'POWR', 'REN', 'XLM', 'UBT', 'WPR']
+crypto = ['BTC', 'ETH', 'ZRX', 'BAL', 'BAT', 'BCH', 'KNC', 'LSK', 'IOTA', 'MRPH', 'NEO', 'ONT', 'DOT', 'POWR', 'REN', 'XLM', 'UBT', 'WPR']
 
-eps_list = []
+df_list = []
 for ticker in crypto:
 	try:
 		API_URL = "https://www.alphavantage.co/query" 
@@ -30,113 +29,24 @@ for ticker in crypto:
 		
 		x = json.dumps(response_json)
 		d = json.loads(x)
-		print(d)
+		e = d['Realtime Currency Exchange Rate']
+		a = dict((k, e[k]) for k in ('1. From_Currency Code', '2. From_Currency Name', '3. To_Currency Code', '4. To_Currency Name', '5. Exchange Rate', '6. Last Refreshed', '7. Time Zone', '8. Bid Price', '9. Ask Price'))	
 
+		df = pd.DataFrame.from_dict(a, orient = 'index')
+		df = df.transpose()
+		df_list.append(df)
+		time.sleep(2)
 	except:
-		pass		
+		pass
 
+final_df = pd.concat(df_list, ignore_index = True)
 
+#print(final_df)
 
+mac_path = r'//Users/alanjackson/Documents/Environments/stocks_env/av_fun_v2.csv'
+win_path = r'C:\\Users\\aljackson\\Documents\\Environments\\py_yfinance\\av_fun_v2.csv'
 
-
-# df_eps = pd.concat(eps_list, ignore_index = True)
-
-# #print(df_eps)
-
-# mac_path = r'//Users/alanjackson/Documents/Environments/stocks_env/AV_EPS_Data.csv'
-# win_path = r'C:\\Users\\aljackson\\Documents\\Environments\\py_yfinance\\AV_EPS_Data.csv'
-
-# #df_eps.to_csv(win_path)
-
-# # Get Fundamentals data for all stocks from Alpha Vantage API (Alpha Vantage API).
-
-# fun_list = []
-# for ticker in unique_tickers:
-# 	try:
-# 		API_URL = "https://www.alphavantage.co/query" 
-# 		data = { 
-# 			"function": 'OVERVIEW', 
-# 			"symbol": ticker,
-# 			"outputsize" : "compact",
-# 			"datatype": "json", 
-# 			"apikey": 'W1U7T09FFM4DY97N'} 
-
-# 		response = requests.get(API_URL, data) 
-# 		response_json = response.json() # maybe redundant
-		
-# 		x = json.dumps(response_json)
-# 		d = json.loads(x)
-# 		subset_d = dict((k, d[k]) for k in ('Symbol', 'Name', 'Sector', 'Industry','FullTimeEmployees', 'FiscalYearEnd', 'LatestQuarter', 'MarketCapitalization', 'EBITDA', 'PERatio', 'PEGRatio', 'BookValue', 'EPS', 'RevenuePerShareTTM', 'ProfitMargin', 'QuarterlyEarningsGrowthYOY', 'QuarterlyRevenueGrowthYOY', 'AnalystTargetPrice', '50DayMovingAverage', '200DayMovingAverage', 'PercentInsiders', 'PercentInstitutions'))
-		
-# 		df = pd.DataFrame.from_dict(subset_d, orient = 'index')
-# 		df = df.transpose()
-# 		fun_list.append(df)
-# 		time.sleep(2)
-# 	except:
-# 		pass
-
-# df_fun = pd.concat(fun_list, ignore_index = True)
-
-# mac_fun_path = r'//Users/alanjackson/Documents/Environments/stocks_env/av_fun_data.csv'
-# win_fun_path = r'C:\\Users\\aljackson\\Documents\\Environments\\py_yfinance\\av_fun_data.csv'
-
-# #df_fun.to_csv(win_fun_path)
-
-# # Get weekly adjusted stock data for all stocks from Alpha Vantage API (Alpha Vantage API)
-
-# master_df = pd.DataFrame()
-# stocks_list = []
-# for ticker in unique_tickers:
-# 	try:
-# 		API_URL = "https://www.alphavantage.co/query" 
-# 		data = { 
-# 			"function": 'TIME_SERIES_WEEKLY_ADJUSTED', 
-# 			"symbol": ticker,
-# 			"outputsize" : "compact",
-# 			"datatype": "json", 
-# 			"apikey": 'W1U7T09FFM4DY97N'} 
-	
-# 		response = requests.get(API_URL, data) 
-# 		response_json = response.json()
-
-# 		x = json.dumps(response_json)
-# 		d = json.loads(x)
-		
-# 		date_list = []
-# 		data_list = []
-
-# 		for line in d['Weekly Adjusted Time Series'].items():
-# 			date, data = line
-# 			date_list.append(date)
-# 			data_list.append(data)
-
-# 		df_date = pd.DataFrame(date_list)
-# 		df_data = pd.DataFrame(data_list)
-
-# 		df = pd.concat([df_date, df_data], axis = 1)
-# 		df['Symbol'] = ticker
-# 		stocks_list.append(df)
-# 		time.sleep(2)
-# 	except:
-# 		pass
-
-# master_df = pd.concat(stocks_list, ignore_index = False)
-# master_df.columns = ['date', 'open', 'high', 'low', 'close', 'adj close', 'volume', 'dividend', 'symbol']
-
-# df_2yr = master_df[(master_df['date'] > '2018-01-01')]
-
-# mac_stocks_path = r'//Users/alanjackson/Documents/Environments/stocks_env/av_weekly_stocks.csv'
-# win_stocks_path = r'C:/Users/aljackson/Documents/Environments/py_yfinance/av_weekly_stocks.csv'
-
-
-# # Join "df_2yr" with "df_fun"
-
-# merged_left = pd.merge(left = df_2yr, right = df_fun, how = 'left', left_on = 'symbol', right_on = 'Symbol')
-
-# mac_stocks_fun_path = r'//Users/alanjackson/Documents/Environments/stocks_env/av_stocks_fun.csv'
-# win_stocks_fun_path = r'C:/Users/aljackson/Documents/Environments/py_yfinance/AV_StocksFundamentals_Merged.csv'
-
-# merged_left.to_csv(win_stocks_fun_path)
+final_df.to_csv(win_path)
 
 # executionTime = (time.time() - startTime)
 # print('Execution time in hours: ' + str(executionTime/3600))
