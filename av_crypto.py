c6412dd101189049fcbd3cfc682ad9290da25333
aj96818 <aj96818@gmail.com>
2021-01-01 16:02:59 -0500

    Ready to run every day on Windows Task Scheduler.

--- av_crypto.py
+++ av_crypto.py
@@ -1,4 +1,4 @@
-# premium api key: W1U7T09FFM4DY97N
+# Premium API Key: W1U7T09FFM4DY97N
 
 import time
 import pandas as pd
@@ -7,9 +7,7 @@ import requests
 import requests_html
 import json
 
-
 startTime = time.time()
-
 
 crypto = ['BTC', 'ETH', 'ZRX', 'BAL', 'BAT', 'BCH', 'KNC', 'LSK', 'IOTA', 'MRPH', 'NEO', 'ONT', 'DOT', 'POWR', 'REN', 'XLM', 'UBT', 'WPR']
 
@@ -25,7 +23,7 @@ for ticker in crypto:
 			"apikey": 'W1U7T09FFM4DY97N'} 
 
 		response = requests.get(API_URL, data) 
-		response_json = response.json() # maybe redundant
+		response_json = response.json() # may be redundant
 		
 		x = json.dumps(response_json)
 		d = json.loads(x)
@@ -34,24 +32,20 @@ for ticker in crypto:
 
 		df = pd.DataFrame.from_dict(a, orient = 'index')
 		df = df.transpose()
+		df = df.drop(['4. To_Currency Name'], axis=1)
 		df_list.append(df)
 		time.sleep(2)
 	except:
 		pass
 
-final_df = pd.concat(df_list, ignore_index = True)
+newdata_df = pd.concat(df_list, ignore_index = True)
 
-#print(final_df)
+df_out = newdata_df
 
-mac_path = r'//Users/alanjackson/Documents/Environments/stocks_env/av_fun_v2.csv'
-win_path = r'C:\\Users\\aljackson\\Documents\\Environments\\py_yfinance\\av_fun_v2.csv'
+mac_path = r'//Users/alanjackson/Documents/Environments/stocks_env/AV_Crypto_Data.csv'
+win_path = r'C:\\Users\\aljackson\\Documents\\Environments\\py_yfinance\\AV_Crypto_Data.csv'
 
-final_df.to_csv(win_path)
+df_out.to_csv(win_path, mode = 'a', index = False, header = False)
 
-# executionTime = (time.time() - startTime)
-# print('Execution time in hours: ' + str(executionTime/3600))
-
-
-
-
-
+executionTime = (time.time() - startTime)
+print('Execution time in minutes: ' + str(executionTime/60))
