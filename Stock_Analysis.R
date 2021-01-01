c0608064a6ee8a53aa2e40658533a5a77a43b005
aj96818 <aj96818@gmail.com>
2021-01-01 12:28:30 -0500

    added two-plot functionality.

--- Stock_Analysis.R
+++ Stock_Analysis.R
@@ -14,19 +14,20 @@ win_wd = 'C:/Users/aljackson/Documents/Environments/py_yfinance/'
 setwd(win_wd)
 
 
-# Read in AV EPS data and transform it from long format to wide format so each stock symbol has one record and it can be neatly joined to Stocks&Fundamentals dataframe.
+# Read in AV EPS data and transform it from long format to wide format so each stock symbol has one record and it can be neatly joined to Stocks & Fundamentals dataframe.
 
-win_av_eps_path <- 'C:/Users/aljackson/Documents/Environments/py_yfinance/AV_EPS_Data.csv'
+
+date <- '2020-12-24'
+win_av_eps_path <- paste0('C:/Users/aljackson/Documents/Environments/py_yfinance/AV_EPS_Report_', date, '.csv')
+
 av_eps <- read_csv(win_av_eps_path, col_types = cols(X1 = col_integer(), reportedDate = col_date(format = "%Y-%m-%d")))
 colnames(av_eps)[1] <- 'index'
-
 
 av_eps$unique_row <- paste0(av_eps$symbol, av_eps$index)
 dupes<-av_eps[c('unique_row')]
 av_eps<-av_eps[!duplicated(dupes),]
 
 eps_list <- split(av_eps, av_eps$symbol)
-
 
 pvt_wide_func <- function(df){
   myvars <- c('unique_row', 'reportedEPS', 'fiscalDateEnding')
@@ -66,7 +67,8 @@ eps_final$eps_2020_09_yr_chg <- eps_final$`2020-09-30` - eps_final$`2019-09-30`
 # Read in AV Stock and Fundamentals data and merge it with transformed EPS dataframe above (eps_final)
 
 mac_av_stocks_path <- "~/Documents/Environments/stocks_env/AV_StocksFundamentals_Merged.csv"
-win_av_stocks_path <- 'C:/Users/aljackson/Documents/Environments/py_yfinance/AV_StocksFundamentals_Merged.csv'
+
+win_av_stocks_path <- paste0('C:/Users/aljackson/Documents/Environments/py_yfinance/AV_Stocks&Fundamentals_Merged_', date, '.csv')
 
 av_stocks <- read_csv(win_av_stocks_path, col_types = cols(LatestQuarter = col_date(format = "%Y-%m-%d"), X1 = col_integer(), date = col_date(format = "%Y-%m-%d")))
 colnames(av_stocks)[1] <- 'Index'
@@ -77,6 +79,10 @@ av_merged$avg_price <- (av_merged$high + av_merged$`adj close` + av_merged$low) 
 av_merged$AvgPriceXVol <- av_merged$avg_price * av_merged$volume
 
 av_merge_split <- split(av_merged, av_merged$symbol)
+
+# Extract of full list to test following function on:
+av_merge_split <- av_merge_split[1:3]
+
 
 # https://www.youtube.com/watch?v=L1J3M9jEWvQ
 
@@ -103,9 +109,9 @@ VMAP_func <- function(df){
   df <- df %>%
     mutate(StDev_4wk = sd(c(avg_price_lag0, avg_price_lag1, avg_price_lag2, avg_price_lag3, avg_price_lag4), na.rm = T))
   df <- df %>%
-    mutate(Upper_BB = VMAP_4wk + (StDev_4wk * 0.75))
+    mutate(Upper_BB = VMAP_4wk + (StDev_4wk * 1))
   df <- df %>%
-    mutate(Lower_BB = VMAP_4wk - (StDev_4wk * 0.75))
+    mutate(Lower_BB = VMAP_4wk - (StDev_4wk * 1))
   
   return(df)
 }
@@ -115,18 +121,39 @@ av_merge_split <- lapply(av_merge_split, VMAP_func)
 av_merged <- ldply(av_merge_split, data.frame)
 
 sq <- av_merged[av_merged$symbol == 'SQ',]
-#write_csv(test, 'test_lag44.csv')
+# write_csv(test, 'test_lag44.csv')
 
 
 p = ggplot() + 
   geom_line(data = sq, aes(x = date, y = `adj.close`), color = "blue") +
-  geom_line(data = sq, aes(x = date, y = VMAP_4Wk), color = "red") +
+  geom_line(data = sq, aes(x = date, y = VMAP_4wk), color = "red") +
   geom_line(data = sq, aes(x = date, y = Upper_BB), color = "black") +
   geom_line(data = sq, aes(x = date, y = Lower_BB), color = "black") +
+  geom_hline(yintercept = min(sq$adj.close)) +
+  #geom_hline(yintercept = max(sq$adj.close)) +
   xlab('Dates') +
   ylab('Adj Close')
 
-print(p)
+
+p3 = ggplot() + 
+  geom_line(data = sq, aes(x = date, y = volume), color = 'purple') +
+  xlab('Dates') +
+  ylab('Volume')
+
+#print(p)
+
+install.packages('gtable')
+library(gtable)
+library(grid)
+g2 <- ggplotGrob(p)
+g3 <- ggplotGrob(p3)
+g <- rbind(g2, g3, size = "first")
+#g$widths <- unit.pmax(g2$widths, g3$widths)
+#grid.newpage()
+#grid.draw(g)
+
+grid.draw(g)
+
 
 
 
